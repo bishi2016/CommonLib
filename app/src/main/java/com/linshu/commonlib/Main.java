ed8d3c05c572aa785a648ffb2757ff95395795d8
shu.lin <381654559@qq.com>
2018-08-23 09:44:17 +0800

    Initial commit

---
+++ app/src/main/java/com/linshu/commonlib/Main.java
@@ -0,0 +1,18 @@
+package com.linshu.commonlib;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by shulin on 2018/8/22.
+ */
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        ScreenAdapterUtil
+    }
+}
